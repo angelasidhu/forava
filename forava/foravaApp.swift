a7f208cc603223e0ba61efe3a2b4acdb698a3d8c
Angela Sidhu <angela02sidhu@gmail.com>
2023-08-01 11:06:07 -0400

    Initial Commit

---
+++ forava/foravaApp.swift
@@ -0,0 +1,17 @@
+//
+//  foravaApp.swift
+//  forava
+//
+//  Created by scholar on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct foravaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
